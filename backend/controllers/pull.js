c898dcc84e04c4e27fa1abdcee1e4cd3fe52c515
Shravani Pujari <shravanipujari2513@gmail.com>
2026-03-09 16:19:04 +0530

    feat(cli): implement pull command to download commits from Supabase

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,5 +1,46 @@
+import fs from "fs/promises";
+import path from "path";
+import supabase from "../supabaseClient.js";
+
 async function pull() {
-    console.log("Pull command called");
+
+    const repoPath = path.resolve(process.cwd(), ".gitfile");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+
+        const { data: files, error } = await supabase.storage
+            .from("repo-storage")
+            .list("", { recursive: true });
+
+        if (error) throw error;
+
+    for (const file of files) {
+
+    if (!file.name.includes(".")) continue; // skip folders
+
+    const { data, error } = await supabase.storage
+        .from("repo-storage")
+        .download(file.name);
+
+    if (error) {
+        console.error("Download error:", error);
+        continue;
+    }
+
+    const localPath = path.join(commitsPath, file.name);
+
+    await fs.mkdir(path.dirname(localPath), { recursive: true });
+
+    const buffer = Buffer.from(await data.arrayBuffer());
+
+    await fs.writeFile(localPath, buffer);
+}
+        console.log("Pull completed successfully");
+
+    } catch (error) {
+        console.error("Pull failed:", error);
+    }
 }
 
 export default pull;
--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -6,7 +6,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-console.log("Supabase URL:", supabaseUrl); // debug
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
